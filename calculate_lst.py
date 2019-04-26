c48da2cafca38ec16c32b507fe6e70a21e7633b7
Aslan Varoqua (Skylines Digital USA) <aslan.varoqua@gmail.com>
2019-04-25 19:03:40 -0600

    Update calculate_lst.py

--- calculate_lst.py
+++ calculate_lst.py
@@ -31,7 +31,7 @@ months='04'
 
 max_cloud_cover=None
 
-path = os.chdir("C:\\\\Users\\avaro\\Desktop\\LST\\" + site_id + "\\")
+path = os.chdir("/mnt/c/Users/avaro/Desktop/LST/" + site_id + "/")
 user = "skylinesgis"
 pass = "pepsiav123pepsiav123"
 
